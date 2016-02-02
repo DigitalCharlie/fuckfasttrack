d45b23992d20dfb3dc2970d5a2b8f8ce369895d1
Aki Rose Braun <hi@akiro.se>
2016-02-02 15:01:44 -0800

    Updates after-action with donate/share modal

--- js/core.js
+++ js/core.js
@@ -3,6 +3,7 @@
 
   var PetitionModalController = BaseShareModalController.extend({
     page_id: 'fuck-the-tpp',
+    petition_content: 'The TPP is bullshit and you have GOT to do something about it.',
     init: function () {
       this.render();
       this.show();
@@ -17,6 +18,7 @@
       modal.classList.add('modal', '_call_modal');
       close.classList.add('close');
       petition.id = 'petition';
+      petition.innerHTML = this.petition_content;
 
       close.textContent = '⨉';
 
@@ -29,13 +31,10 @@
       new EmailPetitionController({
         el: '#petition',
         page_id: this.page_id,
-        onSend: function (formData) {
-          document.getElementById('petition').innerHTML = '<p>Thanks!</p>';
-
-          new CallModalController({
-            campaign: 'cisa-cloture-fax',
-            zip: formData.zip,
-            callScript: 'CISA is really a bad law. Please oppose it!'
+        onSend: function () {
+          new ShareModalController({
+            headline: 'awesome.',
+            text: 'now, share this so more people see how fucking shitty the TPP is. (or, chip in $5 to help us spread the word)'
           });
         }
       });
@@ -45,7 +44,7 @@
 
   doc.querySelector('.button.action').addEventListener('click', function (e) {
     e.preventDefault();
-    new PetitionModalController({page_id: 'fuck-the-tpp'});
+    new PetitionModalController({page_id: 'fuck-the-tpp', petition_content: 'The tpp is bullshit.'});
   });
 
 }(document));
